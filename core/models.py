ed36d7d83f11fc6d88346c35829283abc1e9e4ed
sasha1985y <kuyancev1985@mail.ru>
2024-08-09 23:11:45 +0300

    исправил генерацию уникального айди

--- core/models.py
+++ core/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 import uuid
 
+def generate_unique_id():
+    return uuid.uuid4().hex
+
 class Todo(models.Model):
     name = models.CharField(max_length=200)
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    unique_id = models.CharField(max_length=32, unique=True, default=uuid.uuid4().hex)
+    unique_id = models.CharField(max_length=32, unique=True, default=generate_unique_id)
 
     def __str__(self):
         return self.name
