01348d73706c8666d3e99c9f275affd59b4db4d4
sasha1985y <kuyancev1985@mail.ru>
2024-08-09 22:55:38 +0300

    реализовал генерацию уникального айди

--- core/models.py
+++ core/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+import uuid
 
 class Todo(models.Model):
     name = models.CharField(max_length=200)
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    unique_id = models.CharField(max_length=32, unique=True, default=uuid.uuid4().hex)
 
     def __str__(self):
         return self.name
