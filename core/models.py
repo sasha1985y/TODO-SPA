0513613609be176a730dcda449d72abce9ba042c
sasha1985y <kuyancev1985@mail.ru>
2024-08-10 20:54:53 +0300

    снёс базу и установил заново

--- core/models.py
+++ core/models.py
@@ -1,14 +1,9 @@
 from django.db import models
-import uuid
-
-def generate_unique_id():
-    return uuid.uuid4().hex
 
 class Todo(models.Model):
     name = models.CharField(max_length=200)
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    unique_id = models.CharField(max_length=32, unique=True, default=generate_unique_id)
 
     def __str__(self):
         return self.name
